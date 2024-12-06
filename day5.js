5302f106e5ba9d37d019fedcf167fc64f2a480c3
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-05 20:36:53 -0700

    Update day5.js

--- day5.js
+++ day5.js
@@ -1,30 +1,28 @@
 //let b = [{"before":"47","number":"53"},{"before":"97","number":"13"},{"before":"97","number":"61"},{"before":"97","number":"47"},{"before":"75","number":"29"},{"before":"61","number":"13"},{"before":"75","number":"53"},{"before":"29","number":"13"},{"before":"97","number":"29"},{"before":"53","number":"29"},{"before":"61","number":"53"},{"before":"97","number":"53"},{"before":"61","number":"29"},{"before":"47","number":"13"},{"before":"75","number":"47"},{"before":"97","number":"75"},{"before":"47","number":"61"},{"before":"75","number":"61"},{"before":"47","number":"29"},{"before":"75","number":"13"},{"before":"53","number":"13"}]
 //let l = ["75,47,61,53,29","97,61,53,29,13","75,29,13","75,97,47,61,53","61,13,29","97,13,75,29,47"]
 //b and l are formatted like above
-function part1(b,l) {
+function part1(b, l) {
     let val = 0;
     for (let i = 0; i < l.length; i++) {
         let arr = l[i].split(",");
-        if (isCorrect(arr,b)) {
+        if (isCorrect(arr, b)) {
             val += (arr[arr.length / 2 - .5] - 0)
         }
     }
     return val;
 }
-function part2(b,l) {
+function part2(b, l) {
     let val = 0;
     for (let i = 0; i < l.length; i++) {
         let arr = l[i].split(",");
-        if (!isCorrect(arr,b)) {
-            while (!isCorrect(arr,b)) {
-                arr = sortByProperties(arr,b)
-            }
-            val += (arr[arr.length / 2 - .5] - 0)
+        while (!isCorrect(arr, b)) {
+            arr = sortByProperties(arr, b)
         }
+        val += (arr[arr.length / 2 - .5] - 0)
     }
     return val;
 }
-function isCorrect(arr,b) {
+function isCorrect(arr, b) {
     let isCorrect = true;
     for (let j = 0; j < b.length; j++) {
         if (arr.indexOf(b[j].number) == -1 || arr.indexOf(b[j].before) == -1) {
@@ -36,7 +34,7 @@ function isCorrect(arr,b) {
     }
     return isCorrect;
 }
-function sortByProperties(arr,b) {
+function sortByProperties(arr, b) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
             for (let k = 0; k < b.length; k++) {
