cea37cda4dc8d0e2c901a40ee92e0611c98bc6b3
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-09 09:09:46 -0700

    d9p2

--- day9.js
+++ day9.js
@@ -25,5 +25,46 @@ function part1(str) {
     return val;
 }
 function part2(str) {
-
+    let numlist = str.split("");
+    let files = [];
+    let max = 0;
+    for (let i = 0; i < numlist.length; i++) {
+        for (let j = 0; j < (numlist[i] - 0); j++) {
+            if (i % 2 == 0) {
+                files.push(i / 2);
+                max = Math.max(max,i/2)
+            } else {
+                files.push(".");
+            }
+        }
+    }
+    for (let i = max; i >= 0; i--) {
+      let numoftimes = (files.join(",") + ",").match(new RegExp(i+",","g")).length;
+      let originalindex = files.indexOf(i);
+      let numofperiods = 0;
+      let index = -1;
+      for (let j = 0; j < files.length; j++) {
+        if (files[j] == ".") {
+          numofperiods++
+        } else {
+          numofperiods = 0;
+        }
+        if (numofperiods == numoftimes) {
+          index = j - numofperiods + 1;
+          break;
+        }
+      }
+      if (index == -1) continue;
+      if (index > originalindex) continue;
+      for (let j = 0; j < numoftimes; j++) {
+        files[index + j] = i;
+        files[originalindex + j] = ".";
+      }
+    }
+    let val = 0;
+    for (let i = 0; i < files.length; i++) {
+        if (files[i] == ".") continue;
+        val += (i * (files[i] - 0));
+    }
+    return val;
 }
