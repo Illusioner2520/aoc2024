61654b56500aec7521b4add2de38ffe12052a76a
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-11 11:47:50 -0700

    day 11

---
+++ day11.js
@@ -0,0 +1,42 @@
+//let str = "125 17"
+function part1(str) {
+  let list = str.split(" ");
+  let val = 0;
+  for (let ele of list) {
+    val += getNumAfter(ele-0,25)
+  }
+  return val;
+}
+function part2(str) {
+  let list = str.split(" ");
+  let val = 0;
+  for (let ele of list) {
+    val += getNumAfter(ele-0,75)
+  }
+  return val;
+}
+function getNumAfter(start,iterations) {
+  let s = {}
+  s[start] = 1;
+  let l = 0;
+  for (let i = 0; i < iterations; i++) {
+    let n = {}
+    for (let j in s) {
+      if (j == 0) {
+        n[1] = n[1] ? n[1] + s[j] : s[j]
+      } else if (j.toString().length % 2 === 0) {
+        l = j.toString();
+        n[l.substring(0,l.length/2)-0] = n[l.substring(0,l.length/2)-0] ? n[l.substring(0,l.length/2)-0] + s[j] : s[j]
+        n[l.substring(l.length/2,l.length)-0] = n[l.substring(l.length/2,l.length)-0] ? n[l.substring(l.length/2,l.length)-0] + s[j] : s[j]
+      } else {
+        n[j*2024] = n[j*2024] ? n[j*2024] + s[j] : s[j]
+      }
+    }
+    s=n
+  }
+  let v = 0;
+  for (let key in s) {
+    v += s[key]
+  }
+  return v;
+}
