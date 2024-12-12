0ee22af596935172366d7bc90e7cfb2aed43cf4d
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-12 09:23:58 -0700

    part 1 works, but part 2 still doesnt

--- day12.js
+++ day12.js
@@ -1,54 +1,119 @@
-let l = ["RRRRIICCFF", "RRRRIICCCF", "VVRRRCCFFF", "VVRCCCJFFF", "VVVVCJJCFE", "VVIVCCJJEE", "VVIIICJJEE", "MIIIIIJJEE", "MIIISIJEEE", "MMMISSJEEE"]
-console.log(part1(l))
 function part1(l) {
-    let available = Array.from({ length: (l.length) * (l[0].length) }, (v, k) => k)
-    while (available.length > 0) {
-        let char = l[available[0]];
-        let spacenum = available[0];
-        let coords = toCoordPoints(l, spacenum);
-        let coordList = [coords]
-        let prevCoords = [coords]
-        let area = 0;
-        let perimeter = 0;
-        while (coordList.length > 0) {
-            let c = coordList[0]
-            if (prevCoords.includes(toSpaceNumber(l,c))) continue;
-            area++;
-            if (l[c.y + 1] && l[c.y + 1][c.x] === char) {
-                coordList.push({ x: c.x, y: c.y + 1 })
-                prevCoords.push(toSpaceNumber(l,{ x: c.x, y: c.y + 1 }))
-            } else {
-                perimeter++;
-            }
-            if (l[c.y - 1] && l[c.y - 1][c.x] === char) {
-                coordList.push({ x: c.x, y: c.y - 1 })
-                prevCoords.push(toSpaceNumber(l,{ x: c.x, y: c.y - 1 }))
-            } else {
-                perimeter++;
-            }
-            if (l[c.y][c.x + 1] && l[c.y][c.x + 1] === char) {
-                coordList.push({ x: c.x + 1, y: c.y })
-                prevCoords.push(toSpaceNumber(l,{ x: c.x + 1, y: c.y }))
-            } else {
-                perimeter++;
-            }
-            if (l[c.y][c.x - 1] && l[c.y][c.x - 1] === char) {
-                coordList.push({ x: c.x - 1, y: c.y })
-                prevCoords.push(toSpaceNumber(l,{ x: c.x - 1, y: c.y }))
-            } else {
-                perimeter++;
-            }
-            available.splice(available.indexOf(toSpaceNumber(l, c)), 1)
-            coordList.shift()
-        }
-        console.log("perimeter",char,perimeter);
-        console.log("area",char,area);
+  let available = Array.from({ length: (l.length) * (l[0].length) }, (v, k) => k)
+  let val = 0;
+  while (available.length > 0) {
+    let spacenum = available[0];
+    let coords = toCoordPoints(l, spacenum);
+    let char = l[coords.y][coords.x];
+    let coordList = [coords]
+    let prevCoords = [coords]
+    let area = 0;
+    let perimeter = 0;
+    while (coordList.length > 0) {
+      let c = coordList[0]
+      if (prevCoords.includes(toSpaceNumber(l, c))) {
+        coordList.shift();
+        continue
+      };
+      prevCoords.push(toSpaceNumber(l, c));
+      area++;
+      if (l[c.y + 1] && l[c.y + 1][c.x] == char) {
+        coordList.push({ x: c.x, y: c.y + 1 })
+      } else {
+        perimeter++;
+      }
+      if (l[c.y - 1] && l[c.y - 1][c.x] == char) {
+        coordList.push({ x: c.x, y: c.y - 1 })
+      } else {
+        perimeter++;
+      }
+      if (l[c.y][c.x + 1] && l[c.y][c.x + 1] == char) {
+        coordList.push({ x: c.x + 1, y: c.y })
+      } else {
+        perimeter++;
+      }
+      if (l[c.y][c.x - 1] && l[c.y][c.x - 1] == char) {
+        coordList.push({ x: c.x - 1, y: c.y })
+      } else {
+        perimeter++;
+      }
+      available.splice(available.indexOf(toSpaceNumber(l, c)), 1)
+      coordList.shift()
     }
-    console.log(available)
+    console.log(char,area,perimeter)
+    val += (area * perimeter)
+  }
+  return val;
+}
+function part2(l) {
+  let available = Array.from({ length: (l.length) * (l[0].length) }, (v, k) => k)
+  let val = 0;
+  while (available.length > 0) {
+    let spacenum = available[0];
+    let coords = toCoordPoints(l, spacenum);
+    let char = l[coords.y][coords.x];
+    let coordList = [coords]
+    let prevCoords = [coords]
+    let area = 0;
+    let perimeterList = [];
+    let perimeter = 0;
+    while (coordList.length > 0) {
+      let c = coordList[0]
+      if (prevCoords.includes(toSpaceNumber(l, c))) {
+        coordList.shift();
+        continue
+      };
+      prevCoords.push(toSpaceNumber(l, c));
+      area++;
+      if (l[c.y + 1] && l[c.y + 1][c.x] == char) {
+        coordList.push({ x: c.x, y: c.y + 1 })
+      } else {
+        perimeterList.push(uniqueForPerimeter(l, { x: c.x, y: c.y, dir: 1 }));
+        if (!perimeterList.includes(uniqueForPerimeter(l, { x: c.x - 1, y: c.y, dir: 1 })) && !perimeterList.includes(uniqueForPerimeter(l, { x: c.x + 1, y: c.y, dir: 1 }))) {
+          perimeter++;
+        }
+      }
+      if (l[c.y - 1] && l[c.y - 1][c.x] == char) {
+        coordList.push({ x: c.x, y: c.y - 1 })
+      } else {
+        perimeterList.push(uniqueForPerimeter(l, { x: c.x, y: c.y, dir: 2 }));
+        if (!perimeterList.includes(uniqueForPerimeter(l, { x: c.x - 1, y: c.y, dir: 2 })) && !perimeterList.includes(uniqueForPerimeter(l, { x: c.x + 1, y: c.y, dir: 2 }))) {
+          perimeter++;
+        }
+      }
+      if (l[c.y][c.x + 1] && l[c.y][c.x + 1] == char) {
+        coordList.push({ x: c.x + 1, y: c.y })
+      } else {
+        perimeterList.push(uniqueForPerimeter(l, { x: c.x, y: c.y, dir: 3 }));
+        if (!perimeterList.includes(uniqueForPerimeter(l, { x: c.x, y: c.y - 1, dir: 3 })) && !perimeterList.includes(uniqueForPerimeter(l, { x: c.x, y: c.y + 1, dir: 3 }))) {
+          perimeter++;
+        }
+      }
+      if (l[c.y][c.x - 1] && l[c.y][c.x - 1] == char) {
+        coordList.push({ x: c.x - 1, y: c.y })
+      } else {
+        perimeterList.push(uniqueForPerimeter(l, { x: c.x, y: c.y, dir: 4 }));
+        if (!perimeterList.includes(uniqueForPerimeter(l, { x: c.x, y: c.y - 1, dir: 4 })) && !perimeterList.includes(uniqueForPerimeter(l, { x: c.x, y: c.y + 1, dir: 4 }))) {
+          perimeter++;
+        }
+      }
+      available.splice(available.indexOf(toSpaceNumber(l, c)), 1)
+      coordList.shift()
+    }
+    let sides = 0;
+    //calculate sides here
+    sides = perimeter;
+    console.log(char,area,sides)
+    val += (area * sides)
+  }
+  return val;
 }
 function toCoordPoints(arr, n) {
-    return { x: n % arr[0].length, y: Math.floor(n / arr[0].length) };
+  return { x: n % arr[0].length, y: Math.floor(n / arr[0].length) };
 }
 function toSpaceNumber(arr, n) {
-    return n.y * arr[0].length + n.x;
+  return n.y * arr[0].length + n.x;
+}
+function uniqueForPerimeter(arr, n) {
+  return toSpaceNumber(arr, n) + (100000000 * n.dir);
 }
