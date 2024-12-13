65d034d1a301a417070ec80754e4062d9ed0deef
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-13 08:52:01 -0700

    part 2 done :)

--- day13.js
+++ day13.js
@@ -19,9 +19,25 @@ function part1(l) {
                 continue;
             }
             val += (3 * j) + nb;
-            console.log(j,nb)
             break;
         }
     }
     return val;
+}
+function part2(l) {
+  let val = 0;
+  for (let i = 0; i < l.length / 4; i++) {
+    let t = i * 4;
+    let a1 = l[t].match(/\d+/g);
+    let b1 = l[t + 1].match(/\d+/g);
+    let goal = l[t + 2].match(/\d+/g);
+    goal[0] = goal[0] - 0 + 10000000000000;
+    goal[1] = goal[1] - 0 + 10000000000000;
+    let x = (goal[0] * b1[1] - b1[0] * goal[1]) / (a1[0] * b1[1] - a1[1] * b1[0]);
+    let y = (goal[0] - (a1[0] * x)) / b1[0];
+    if (x == null || y == null) continue;
+    if (x % 1 != 0 || y % 1 != 0) continue;
+    val += (3 * x) + y
+  }
+  return val;
 }
