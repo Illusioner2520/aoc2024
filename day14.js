b0795f7ed511530a637b6b4272a284b6288a65b1
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-14 15:04:03 -0700

    format day 14 in vscode

--- day14.js
+++ day14.js
@@ -1,6 +1,6 @@
 //let l = ["p=0,4 v=3,-3","p=6,3 v=-1,-3","p=10,3 v=-1,2","p=2,0 v=2,-1","p=0,0 v=1,3","p=3,0 v=-2,-2","p=7,6 v=-1,-3","p=3,0 v=-1,-2","p=9,3 v=2,3","p=7,3 v=-1,2","p=2,4 v=2,-3","p=9,5 v=-3,-3"]
 function part1(l) {
-    let q = [0,0,0,0]
+    let q = [0, 0, 0, 0]
     for (let i of l) {
         let nums = i.match(/[-\d]+/g)
         let x = (nums[0] - 0 + (nums[2] * 100) + 101000) % 101
@@ -27,14 +27,14 @@ function part2(l) {
     //  </body>
     //</html>
     for (let i = 1; i < 8000; i++) {
-        let url = generateBoard(l,i);
+        let url = generateBoard(l, i);
         let img = document.createElement("img");
         img.src = url;
         img.title = i;
         document.getElementsByTagName('body')[0].appendChild(img)
     }
 }
-function generateBoard(l,seconds) {
+function generateBoard(l, seconds) {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = 103;
