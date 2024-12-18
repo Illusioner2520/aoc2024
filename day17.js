afbced94812358171a90cd65d7ab0fe5ff8beaba
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2024-12-17 19:18:55 -0700

    first day i had to consult other people's solutions

--- day17.js
+++ day17.js
@@ -2,8 +2,9 @@
 //let b = 0n;
 //let c = 0n;
 //let program = "0,3,5,4,3,0"
-let l = program.split(",");
-function part1(a,b,c,l) {
+//let l = program.split(",");
+
+function part1(a, b, c, l) {
     let index = 0;
     let output = [];
     while (index < l.length) {
@@ -11,11 +12,11 @@ function part1(a,b,c,l) {
         let v = l[index] - 0;
         let x = l[index + 1] - 0;
         if (v == 0) {
-            a = a >> combo(a,b,c,x)
+            a = a >> combo(a, b, c, x)
         } else if (v == 1) {
             b = b ^ BigInt(x);
         } else if (v == 2) {
-            b = combo(a,b,c,x) % 8n;
+            b = combo(a, b, c, x) % 8n;
         } else if (v == 3) {
             if (a != 0) {
                 index = x;
@@ -24,35 +25,35 @@ function part1(a,b,c,l) {
         } else if (v == 4) {
             b = b ^ c;
         } else if (v == 5) {
-            output.push(combo(a,b,c,x) % 8n)
+            output.push(combo(a, b, c, x) % 8n)
         } else if (v == 6) {
-            b = a >> combo(a,b,c,x)
+            b = a >> combo(a, b, c, x)
         } else if (v == 7) {
-            c = a >> combo(a,b,c,x)
+            c = a >> combo(a, b, c, x)
         }
         if (incrementIndex) index += 2;
     }
     return output.join(",")
 }
-function part2(b,c,l) {
+function part2(b, c, l) {
     // I brute forced it up to 1 billion then gave up on brute forcing
     // I looked at other people's solutions to figure it out
     // Don't quite understand it
     // shoutout to u/genabasov and u/BIGJRA on r/adventofcode, whose solutions helped me come up with this one.
-    return search(0n,0,b,c,structuredClone(l),l.reverse())
+    return search(0n, 0, b, c, structuredClone(l), l.reverse())
 }
-function search(input,index,b,c,l1,l) {
+function search(input, index, b, c, l1, l) {
     if (index == l.length) return input;
     for (let i = 7n; i >= 0n; i--) {
         let a = input * 8n + i;
-        let v = part1(a,b,c,l1).split(",")
+        let v = part1(a, b, c, l1).split(",")
         if (v[0] == l[index]) {
-            let min = search(a,index + 1,b,c,l1,l);
+            let min = search(a, index + 1, b, c, l1, l);
             if (min) return min;
         }
     }
 }
-function combo(a,b,c,x) {
+function combo(a, b, c, x) {
     if (x < 4) return BigInt(x);
     if (x == 4) return a;
     if (x == 5) return b;
